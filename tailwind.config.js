fd4035417f696117f583ef7a3e556ce88b50d313
Ilhan Haffiyan Juldan <ilhanzuldan11@gmail.com>
2024-12-20 06:33:25 +0700

    revision
    add: feature other projects page
    fix: some error and components
    add: design thinking process

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./index.html", "./other-projects.html"],
   darkMode: "class",
   theme: {
     extend: {
